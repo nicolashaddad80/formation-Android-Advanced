58aca873afe5a7abf97ef638e3fc3c883c8b9600
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-28 14:51:36 +0200

    simple UI fragment

---
+++ FormationAndroid/app/src/main/java/com/example/formationandroid/SampleFragment.java
@@ -0,0 +1,23 @@
+package com.example.formationandroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SampleFragment extends Fragment {
+
+    public SampleFragment() {
+        // Required empty public constructor
+        super(R.layout.fragment_sample);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}

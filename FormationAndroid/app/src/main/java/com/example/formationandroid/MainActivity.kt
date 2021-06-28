b28ab28695ff572a144c1173dcefa1e3fa1ac4a7
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-28 15:42:52 +0200

    Dynamic set of UI fragment

--- FormationAndroid/app/src/main/java/com/example/formationandroid/MainActivity.kt
+++ FormationAndroid/app/src/main/java/com/example/formationandroid/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.formationandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener {
+            //Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment,SampleFragment(), null)
+                .commit()
+        }
     }
 }

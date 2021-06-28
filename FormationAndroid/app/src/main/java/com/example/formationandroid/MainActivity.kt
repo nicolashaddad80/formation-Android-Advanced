fbcc09ac914657b6f9463a8012dc81df41d160c0
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-28 10:20:34 +0200

    Linear-Layout

---
+++ FormationAndroid/app/src/main/java/com/example/formationandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.formationandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_sample)
+    }
+}
